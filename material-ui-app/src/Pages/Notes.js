17cc403d55b4d7a12037b8a3b97b940fd67f05d9
A-Jha <avvinashjha@gmail.com>
2021-07-13 15:44:30 +0530

    some prev code

--- material-ui-app/src/Pages/Notes.js
+++ material-ui-app/src/Pages/Notes.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 export const Notes = () => {
   const [notes, setNotes] = React.useState([]);
 
@@ -12,15 +13,36 @@ export const Notes = () => {
   return (
     <>
       <div>
-        {notes.map((note) => {
-          return (
-            <div key={note.id}>
-              <h3>{note.title}</h3>
-              <p>{note.details}</p>
-              <h4>{note.category}</h4>
-            </div>
-          );
-        })}
+        {/* <Grid container alignItems="center" direction="row-reverse">
+          <Grid item xs={3} md={2} alignContent="center">
+            1
+          </Grid>
+          <Grid item xs={3} md={2}>
+            2
+          </Grid>
+          <Grid item xs={3} md={2}>
+            3
+          </Grid>
+          <Grid item xs={3} md={2}>
+            4
+          </Grid>
+          <Grid item xs={3} md={2}>
+            5
+          </Grid>
+        </Grid> */}
+        <Grid container justifyContent="center" spacing={2}>
+          {notes.map((note) => {
+            return (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={note.id}>
+                <Paper style={{ padding: 20 }}>
+                  <h3>{note.title}</h3>
+                  <p>{note.details}</p>
+                  <h4>{note.category}</h4>
+                </Paper>
+              </Grid>
+            );
+          })}
+        </Grid>
       </div>
     </>
   );
